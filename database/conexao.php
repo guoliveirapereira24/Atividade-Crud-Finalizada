f156f672d4115730e30833beccf224863bd721f8
GUSTAVO DE OLIVEIRA PEREIRA <21193137@CFP127ALUNO.LOCAL>
2021-11-19 11:01:04 -0300

    Corrigindo nome do banco

--- database/conexao.php
+++ database/conexao.php
@@ -3,7 +3,7 @@
 const HOST = 'localhost';
 const USER = 'root';
 const PASSWORD = 'bcd127';
-const DATABASE = 'db_cadastro';
+const DATABASE = 'cadastro';
 
 $conexao = mysqli_connect(HOST, USER, PASSWORD, DATABASE);
 
